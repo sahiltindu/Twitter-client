fffbdeaf72488ebc19883d4912756d881f434957
sahiltindu <ss2323155@gmail.com>
2024-04-18 22:38:46 +0530

    modularised code

--- gql/gql.ts
+++ gql/gql.ts
@@ -13,6 +13,8 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
+    "#graphql\nmutation createTweet($payload: CreateTweetData!) {\n  createTweet(payload: $payload) {\n   id \n   \n  }\n}\n": types.CreateTweetDocument,
+    "#graphql\n\nquery GetAllTweets {\n  getAllTweets {\n    id\n    content\n    imageURL\n    author{\n      firstName\n      lastName\n      profileImageURL\n    }\n  }\n}\n\n": types.GetAllTweetsDocument,
     "\n#graphql\nquery VerifyUserGoogleToken($token: String!){\n  verifyGoogleToken(token: $token)\n}\n": types.VerifyUserGoogleTokenDocument,
     "\nquery GetCurrentUser {\n  getCurrentUser {\n    id\n    profileImageURL\n    email\n    firstName\n    lastName\n  }\n}": types.GetCurrentUserDocument,
 };
@@ -31,6 +33,14 @@ const documents = {
  */
 export function graphql(source: string): unknown;
 
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "#graphql\nmutation createTweet($payload: CreateTweetData!) {\n  createTweet(payload: $payload) {\n   id \n   \n  }\n}\n"): (typeof documents)["#graphql\nmutation createTweet($payload: CreateTweetData!) {\n  createTweet(payload: $payload) {\n   id \n   \n  }\n}\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "#graphql\n\nquery GetAllTweets {\n  getAllTweets {\n    id\n    content\n    imageURL\n    author{\n      firstName\n      lastName\n      profileImageURL\n    }\n  }\n}\n\n"): (typeof documents)["#graphql\n\nquery GetAllTweets {\n  getAllTweets {\n    id\n    content\n    imageURL\n    author{\n      firstName\n      lastName\n      profileImageURL\n    }\n  }\n}\n\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
